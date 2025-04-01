587b965dd1d5f9676bfc152f4be8378b23321a2f
julio96correa <julio96correa@gmail.com>
2025-03-31 20:14:26 -0500

    feat: added test cases

--- laboratorio1/lsl_punto1.py
+++ laboratorio1/lsl_punto1.py
@@ -73,6 +73,7 @@ class LSL:
         nodo_derecho = derecha.cabecera
         
         while nodo_izquierdo and nodo_derecho:
+            
             if nodo_izquierdo.valor < nodo_derecho.valor:
                 lista_ordenada.insertar(nodo_izquierdo.valor)
                 nodo_izquierdo = nodo_izquierdo.siguiente
@@ -91,11 +92,18 @@ class LSL:
         return lista_ordenada
 
 def main():
+    
+    # Prueba 1:
+    # Lista_1 = 7 -> 8 -> 8 -> 1 -> NULL
+    # Lista_2 = 2 -> 9 -> 1 -> 4 -> NULL
+
+    print("Prueba 1")
     Lista_1 = LSL()
     Lista_1.insertar(7)
     Lista_1.insertar(8)
     Lista_1.insertar(8)
     Lista_1.insertar(1)
+    Lista_1 = Lista_1.merge_sort()
     print("Lista 1")
     Lista_1.mostrar()
     
@@ -107,6 +115,66 @@ def main():
     print("Lista 2")
     Lista_2.mostrar()
     
+    # Colocar la función principal para solucionar y la respuesta generada
+    Lista_concatenada = LSL.concatenar(Lista_1, Lista_2)
+    print("Lista concatenada:")
+    Lista_concatenada.mostrar()
+    Lista_concatenada = Lista_concatenada.merge_sort()
+    print("Lista ordenada:")
+    Lista_concatenada.mostrar()
+    
+    # Prueba 2:
+    # Lista_1 = 10 -> 9 -> 8 -> 7 -> NULL
+    # Lista_2 = 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+
+    print("\nPrueba 2")
+    Lista_1 = LSL()
+    Lista_1.insertar(10)
+    Lista_1.insertar(9)
+    Lista_1.insertar(8)
+    Lista_1.insertar(7)
+    print("Lista 1")
+    Lista_1.mostrar()
+
+    Lista_2 = LSL()
+    Lista_2.insertar(6)
+    Lista_2.insertar(5)
+    Lista_2.insertar(4)
+    Lista_2.insertar(3)
+    Lista_2.insertar(2)
+    Lista_2.insertar(1)
+    print("Lista 2")
+    Lista_2.mostrar()
+
+    # Colocar la función principal para solucionar y la respuesta generada
+    Lista_concatenada = LSL.concatenar(Lista_1, Lista_2)
+    print("Lista concatenada:")
+    Lista_concatenada.mostrar()
+    Lista_concatenada = Lista_concatenada.merge_sort()
+    print("Lista ordenada:")
+    Lista_concatenada.mostrar()
+    
+    # Prueba 3:
+    # Lista_1 = 5 -> 5 -> 5 -> 5 -> NULL
+    # Lista_2 = 6 -> 6 -> 6 -> NULL
+
+    print("\nPrueba 3")
+    Lista_1 = LSL()
+    Lista_1.insertar(5)
+    Lista_1.insertar(5)
+    Lista_1.insertar(5)
+    Lista_1.insertar(5)
+    print("Lista 1")
+    Lista_1.mostrar()
+
+    Lista_2 = LSL()
+    Lista_2.insertar(6)
+    Lista_2.insertar(6)
+    Lista_2.insertar(6)
+    print("Lista 2")
+    Lista_2.mostrar()
+
+    # Colocar la función principal para solucionar y la respuesta generada
     Lista_concatenada = LSL.concatenar(Lista_1, Lista_2)
     print("Lista concatenada:")
     Lista_concatenada.mostrar()
